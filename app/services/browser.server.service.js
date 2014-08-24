768c38440752f3be6ef26139c2f4a50bffd9e475
Yosub Shin <yosub@yosubshin.com>
2014-08-24 00:46:23 -0500

    Integrated Spooky.js
    Injected script gets executed. However, it keep causes parse error

---
+++ app/services/browser.server.service.js
@@ -0,0 +1,118 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var util = require('util'),
+    Q = require('q');
+
+var database = {};
+var deferredMap = {};
+
+try {
+    var Spooky = require('spooky');
+} catch (e) {
+    var Spooky = require('../lib/spooky');
+}
+
+var theScript = 
+        '(function(){console.log(\'Here I am the injected script! \' + document.location);' + 
+        'console.log(\'database : \' + JSON.stringify(database));' +
+        'database.newKey = \'New Value\';' +
+        'return null;})();';
+
+
+exports.run = function(script, trigger) {
+    console.log('Running script %j, for trigger %j', script, trigger);
+    var deferred = Q.defer();
+    var spooky = new Spooky({
+        child: {
+            transport: 'http'
+        },
+        casper: {
+            logLevel: 'debug',
+            verbose: true
+        }
+    }, function (err) {
+        if (err) {
+            console.error('Unable to initialize SpookyJS, %j', err);
+            var e = new Error('Failed to initialize SpookyJS');
+            e.details = err;
+            throw e;
+        }
+
+        // No error initializing SpookyJS
+        if (!database[script._id]) {
+            database[script._id] = {};
+        }
+        if (deferredMap[script._id]) {
+            throw new Error('Does not expect deferred object for script id exists, but it is!');
+        }
+        deferredMap[script._id] = deferred;
+
+
+        spooky.start(script.url);
+        spooky.then([{
+            script: script,
+            database: database[script._id]
+        }, function () {
+            // CasperJS's context
+            var ret = this.evaluate(function (script, database) {
+                // Client browser context
+                try {
+                    return {
+                        result: eval(script),
+                        database: database
+                    };
+                } catch (err) {
+                    console.log('Error in browser : ' + err);
+                    return {error : err};
+                }
+            }, {script: script.content, database: database});
+            this.emit('scriptIsDone', ret, script._id);
+        }]);
+        spooky.run();
+     });
+
+    spooky.on('error', function (e, stack) {
+        console.error(e);
+
+        if (stack) {
+            console.log(stack);
+        }
+    });
+
+    spooky.on('console', function (line) {
+        console.log(line);
+    });
+
+    spooky.on('log', function (log) {
+        if (log.space === 'remote') {
+            console.log(log.message.replace(/ \- .*/, ''));
+        }
+    });
+
+    spooky.on('remote.message', function (msg) {
+        console.log('[browser] ' + msg);
+    });
+
+    spooky.on('scriptIsDone', function(result, scriptId) {
+        if (!scriptId || !deferredMap[scriptId]) {
+            console.error('Script is done executing, but script does not exist. scriptId=%j', scriptId);
+            return;
+        }
+        var deferred = deferredMap[scriptId];
+        delete deferredMap[scriptId];
+        console.log('Result from script :\n%j', result);
+        if (!result) {
+            deferred.reject('Result does not exists for executed script, %j', scriptId);
+        } else if (result.error) {
+            deferred.reject('Error occurred while executing the script. %j', result.error);
+        } else if (!result.result || !result.database) {
+            deferred.reject('result or database field does exists for executed script, %j', scriptId);
+        } else {
+            deferred.resolve(result.result);
+        }        
+    });
+    return deferred.promise;
+};
--- app/services/triggers.server.service.js
+++ app/services/triggers.server.service.js
@@ -8,7 +8,8 @@ var mongoose = require('mongoose-q')(require('mongoose')),
     Trigger = mongoose.model('Trigger'),
     util = require('util'),
     Q = require('q'),
-    _ = require('lodash');
+    _ = require('lodash'),
+    browser = require('../../app/services/browser');
 
 function enqueue(rawTrigger) {    
     console.log('Engueuing trigger %j', rawTrigger);
@@ -29,33 +30,50 @@ exports.enqueue = enqueue;
 function execute(trigger) {
     console.log('Executing trigger %j', trigger);
     var deferred = Q.defer();
+    var script;
     // do something that takes some time
 
-    //TODO resolve, or reject deferred object
-    //TODO Do execute script
-    console.log('TODO This is supposed to be actual work.');
-    setTimeout(function() {
-        trigger.state = 'finished';
-        trigger.saveQ()
-            .then(function(result) {
-                return Script.findByIdQ(trigger.script);
-            })
-            .then(function(script) {
-                return enqueue({
-                    script: trigger.script, 
-                    state: 'created', 
-                    triggerAt: Date.now() + script.period * 1000, 
-                    active: true
+    Script.findByIdQ(trigger.script)
+        .then(function(result) {
+            script = result;            
+            console.log('Running browser script %j', script);
+            return browser.run(script, trigger).timeout(10000);
+        })
+        .then(function(result) {
+            console.log('Finished running script with result %j', result);
+            trigger.state = 'finished';
+            return trigger.saveQ();
+        })
+        .then(function() {
+            return enqueue({
+                script: trigger.script, 
+                state: 'created', 
+                triggerAt: Date.now() + script.period * 1000, 
+                active: true
+            });
+        })
+        .then(function(result) {
+            deferred.resolve(trigger);
+        })
+        .fail(function(err) {
+            console.log('Error while executing trigger.', err);
+            trigger.state = 'failed';
+            trigger.saveQ()
+                .then(function() {
+                    script.active = false;
+                    return script.saveQ();
+                })
+                .then(function() {
+                    console.log('Updated Script to inactive state');
+                })
+                .fail(function() {
+                    console.error('This should not happen');
+                })
+                .done(function() {
+                    deferred.reject(err);
                 });
-            })
-            .then(function(result) {
-                deferred.resolve(trigger);
-            })
-            .fail(function(err) {
-                deferred.reject(err);
-            })
-            .done();
-    }, 1);
+        })
+        .done();
 
     return deferred.promise;
 }
